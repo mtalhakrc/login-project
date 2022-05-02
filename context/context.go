6d96c6ea4db5078d8b5959249ff2f6df3c5e106a
MTalhaKaraca <89966125+MTalhaKaraca@users.noreply.github.com>
2022-05-02 22:48:01 +0300

    fixed context

--- context/context.go
+++ context/context.go
@@ -7,15 +7,15 @@ import (
 )
 
 type AppCtx struct {
-	C  *fiber.Ctx
+	*fiber.Ctx
 	DB *gorm.DB
 }
 
 func CtxWrap(h func(ctx *AppCtx) error) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		return h(&AppCtx{
-			C:  c,
-			DB: database.DB(),
+			Ctx: c,
+			DB:  database.DB(),
 		})
 	}
 }
